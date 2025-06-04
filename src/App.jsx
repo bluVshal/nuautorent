59c22edbba904c56f97a937f0319226e10b6ae4b
bluVshal <blu.vishal@gmail.com>
2025-06-04 11:04:16 +0400

    Added Routing in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NavMenu from './components/navmenu';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-       </div>
+      <NavMenu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
     </>
   )
 }
--- src/components/navmenu/index.jsx
+++ src/components/navmenu/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const NavMenu = () => {
   return (
     <div>
-      <p>NavMenu</p>
+    <Link to="/">Go to Home</Link>
+      <Link to="/about">Go to About</Link>
     </div>
   )
 }
