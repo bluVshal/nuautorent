73b37e308f9a42cac706e5b16a8de0c160b89e22
bluVshal <blu.vishal@gmail.com>
2025-06-05 09:12:38 +0400

    Added nav menu css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import NavMenu from './components/navmenu';
+import './App.css';
 
 function App() {
 
   return (
-    <>
+    <div class="root-container">
       <NavMenu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
 
-    </>
+    </div>
   )
 }
 
--- src/components/navmenu/index.jsx
+++ src/components/navmenu/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './navmenu.css';
 
 const NavMenu = () => {
+  const menuItemsArr = [
+    { "link": "/", "title": "Home" },
+    { "link": "/about", "title": "About" }
+  ];
+
   return (
     <div>
-    <Link to="/">Go to Home</Link>
-      <Link to="/about">Go to About</Link>
+      {menuItemsArr.map((mnu) => {
+        return (
+          <>
+            <Link key={mnu.title} to={mnu.link}> {mnu.title} </Link>
+          </>
+        );
+      })}
+
+
     </div>
   )
 }
