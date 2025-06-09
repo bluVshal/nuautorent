37db2e0bef2250f275206234797fc4d384f41f1f
bluVshal <blu.vishal@gmail.com>
2025-06-09 23:45:47 +0400

    Added first implementation of login form

--- src/components/login/loginModal.jsx
+++ src/components/login/loginModal.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 
 const LoginModal = () => {
   return (
     <div>
       <p> LOGIN HERE! </p>
+      
+      <div className="inline-flex flex-column gap-2">
+        <label htmlFor="username" className="text-primary-50 font-semibold">
+          Username
+        </label>
+        <InputText id="username" label="Username" className="bg-white-alpha-20 border-none p-3 text-primary-50"></InputText>
+      </div>
+
+      <div className="inline-flex flex-column gap-2">
+        <label htmlFor="password" className="text-primary-50 font-semibold">
+          Password
+        </label>
+        <InputText id="password" label="Password" className="bg-white-alpha-20 border-none p-3 text-primary-50" type="password"></InputText>
+      </div>
+
+      <div className="flex align-items-center gap-2">
+        <Button label="Sign-In" onClick={(e) => hide(e)} text className="p-3 w-full text-primary-50 border-1 border-white-alpha-30 hover:bg-white-alpha-10"></Button>
+        <Button label="Cancel" onClick={(e) => hide(e)} text className="p-3 w-full text-primary-50 border-1 border-white-alpha-30 hover:bg-white-alpha-10"></Button>
+      </div>
+
     </div>
   )
 }
