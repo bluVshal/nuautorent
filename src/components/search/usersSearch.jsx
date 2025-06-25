fd4e4c481f18ec8e0f744368c4e8697df6c9070c
bluVshal <blu.vishal@gmail.com>
2025-06-25 09:23:20 +0400

    User Search scaffold implemented

--- src/components/search/usersSearch.jsx
+++ src/components/search/usersSearch.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSomeUsers } from '../../api/slices/usersSlice';
 
 const UsersSearch = () => {
+    const dispatch = useDispatch();
+    const [t, i18n] = useTranslation("global");
+    const [isFormReset, setIsFormReset] = useState('true');
+    const usersStatus = useSelector(state => state.users.status);
+    const resetAll = () => {
+
+    };
+    const searchUser = () => {
+        setIsFormReset(false);
+        dispatch(fetchSomeUsers());
+    };
+
+
     return (
         <div>
             <div className='search-main-container'>
                 <h4 className='header-text'> Users Search </h4>
-                <div className='search-item-container'>
-
-                </div>
+                <Button raised label={usersStatus === 'loading' ? 'Searching...' : 'Search'} disabled={usersStatus === 'loading'} onClick={searchUser} />
+                <Button label={t('buttons.reset')} disabled={usersStatus === 'loading'} onClick={resetAll}></Button>
             </div>
         </div>
     )
