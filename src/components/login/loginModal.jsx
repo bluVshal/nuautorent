97c8c2a91c0e3f246a121b2c4dcd77cd1ff4e7a1
bluVshal <blu.vishal@gmail.com>
2025-06-27 21:26:17 +0400

    Corrected Login and SignUp parts

--- src/components/login/loginModal.jsx
+++ src/components/login/loginModal.jsx
@@ -1,30 +1,32 @@
 import React, { useRef } from 'react'
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
+import { useTranslation } from 'react-i18next';
 import './login.css'
 
 const LoginModal = () => {
   const uNameInput = useRef(null);
+    const [t, i18n] = useTranslation("global");
 
   return (
     <div className='items-container'>
       <div className='text-container'>
         <label className='lbl-text' htmlFor="username">
-          Username
+          {t('signUp.username')}
         </label>
-        <InputText ref={uNameInput} autoFocus id="username" label="Username" ></InputText>
+        <InputText className='txt-search-item'ref={uNameInput} autoFocus id="username" label="Username" ></InputText>
       </div>
 
       <div className='text-container'>
         <label className='lbl-text' htmlFor="password">
-          Password
+          {t('signUp.password')}
         </label>
-        <InputText id="password" label="Password" type="password"></InputText>
+        <InputText className='txt-search-item' id="password" label="Password" type="password"></InputText>
       </div>
 
       <div className="btn-container">
-        <Button label="Sign-In" onClick={(e) => hide(e)} text className="btn-submit"></Button>
-        <Button label="Cancel" onClick={(e) => hide(e)} text className="btn-cancel"></Button>
+        <Button label={t('signUp.signIn')} onClick={(e) => hide(e)} text className="btn-submit"></Button>
+        <Button label={t('signUp.cancel')} onClick={(e) => hide(e)} text className="btn-cancel"></Button>
       </div>
 
     </div>
--- src/components/login/signup.jsx
+++ src/components/login/signup.jsx
@@ -1,37 +1,39 @@
 import React, { useRef } from 'react';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
+import { useTranslation } from 'react-i18next';
 import './login.css'
 
 const SignIn = () => {
   const uNameInput = useRef(null);
+  const [t, i18n] = useTranslation("global");
 
   return (
     <div className='items-container'>
       <div className="text-container">
-        <label htmlFor="username" className='lbl-text'>
-          Username
+        <label htmlFor="username" className='lbl-search-item'>
+          {t('signUp.username')}
         </label>
-        <InputText ref={uNameInput} autoFocus id="username" label="Username" className=""></InputText>
+        <InputText className='txt-search-item' ref={uNameInput} autoFocus id="username" label="Username"></InputText>
       </div>
 
       <div className="text-container">
         <label htmlFor="password" className='lbl-text'>
-          Password
+          {t('signUp.password')}
         </label>
-        <InputText id="password" label="Password" className="" type="password"></InputText>
+        <InputText className='txt-search-item' id="password" label="Password" type="password"></InputText>
       </div>
 
       <div className="text-container">
         <label htmlFor="cpassword" className='lbl-text'>
-          Confirm Password
+          {t('signUp.confirmPassword')}
         </label>
-        <InputText id="cpassword" label="Confirm Password" className="" type="password"></InputText>
+        <InputText className='txt-search-item' id="cpassword" label="Confirm Password" type="password"></InputText>
       </div>
 
       <div className="btn-container">
-        <Button label="Sign-In" onClick={(e) => hide(e)} text className="btn-submit"></Button>
-        <Button label="Cancel" onClick={(e) => hide(e)} text className="btn-cancel"></Button>
+        <Button label={t('signUp.signUp')} onClick={(e) => hide(e)} text className="btn-submit"></Button>
+        <Button label={t('signUp.cancel')} onClick={(e) => hide(e)} text className="btn-cancel"></Button>
       </div>
 
     </div>
